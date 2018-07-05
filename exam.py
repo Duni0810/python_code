eca14c524e738363f6ab2a0a887e5c89c39f028e
young <1486084728@qq.com>
2018-07-05 06:19:58 -0700

    add exam file

---
+++ exam.py
@@ -0,0 +1,25 @@
+
+#coding=utf-8
+#Str, 请反转字符串
+
+
+testname = "hello world"
+
+print testname[::-1]
+
+print testname[::-2]
+
+#（面试题）给定一个字符串aStr，返回使用空格或者'\t'分割后的倒数第二个子串
+
+teststr = "young\t is \n god\t"
+
+print teststr.split()
+
+
+
+
+
+
+
+
+
