b939abb37a33f44c9aa0c867d45e41702362b40a
young <1486084728@qq.com>
2018-07-05 08:25:22 -0700

    updata exam.py

--- exam.py
+++ exam.py
@@ -9,17 +9,33 @@ print testname[::-1]
 
 print testname[::-2]
 
-#（面试题）给定一个字符串aStr，返回使用空格或者'\t'分割后的倒数第二个子串
+#给定一个字符串aStr，返回使用空格或者'\t'分割后的倒数第二个子串
 
 teststr = "young\t is \n god\t"
 
 print teststr.split()
 
 
+#一个学校，有3个办公室，现在有8位老师等待工位的分配，请编写程序，完成随机的分配
 
+import random
 
+#定义一个列表用来保存3个办公室
+offices = [[], [], []]
+#定义一个列表用来保存8个老师
+names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
+for name in names :
+	index = random.randint(0, 2)
+	offices[index].append(name)
+i = 1
+for tempName in offices :
+	print '办公室%d有%d个人'%(i, len(tempName))
+	i += 1
 
-
+	for name in tempName :
+		print '%s'%name,
+	print ''
+	print '-'*20
 
 
