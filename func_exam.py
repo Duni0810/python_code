2491aa94a2c65a0a0ebf780a1940439574eb2849
young <1486084728@qq.com>
2018-07-06 07:01:02 -0700

    change file

---
+++ func_exam.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+
+#2.用函数实现求100-200里面所有的素数
+#提示：素数的特征是除了1和其本身能被整除，其它数都不能被整除的数
+
+import math
+
+def myprime(start , end):
+	
+	print 'start'
+	while start < end :
+
+		i = 2
+		while i < start:
+
+			if start%i == 0 :
+				break
+			i = i + 1
+
+		if start <= i :
+			print start
+		start = start + 1
+
+myprime(100, 200)
