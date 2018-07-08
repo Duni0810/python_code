6387d2eb832fc54263b8ccd2fab64d68fcd7f037
young <1486084728@qq.com>
2018-07-08 04:34:13 -0700

    modified func_exam

--- func_exam.py
+++ func_exam.py
@@ -21,4 +21,22 @@ def myprime(start , end):
 			print start
 		start = start + 1
 
+#请用函数实现一个判断用户输入的年份是否是闰年的程序
+#1.能被400整除的年份 
+#2.能被4整除，但是不能被100整除的年份
+#以上2种方法满足一种即为闰年
+
+def myleap(year) :
+	if year % 400 == 0 :
+		print 'leap year'
+		return 1
+	elif year % 4 == 0 and year % 100 != 0 :
+		print 'leap year'
+		return 1
+	print 'no leap year'
+	return 0
+
+
+
 myprime(100, 200)
+myleap(2004)
